05d6c11926bcfbc4e030837986e3df9c657405d0
Slepabot <KuzVL2006@yandex.ru>
2026-06-24 01:42:35 +0300

    Created some sort of basic neuron with fixed step

--- Preps for creating AI/Neuron.py
+++ Preps for creating AI/Neuron.py
@@ -2,16 +2,32 @@ import random
 
 class Neuron:
     def __init__(self):
-        self.w = 0
+        self.y = None
+        self.w = random.uniform(-0.25, 0.25)
         self.x = 0
         self.b = 0
 
-    def create(self, x, bias):
-        self.w = random.uniform(-0.25, 0.25)
+    def think(self, x, bias):
         self.x = x
         self.b = bias
+        self.y = self.w * self.x + self.b
+        return self.y
 
-    def answer(self):
-        return self.w * self.x + self.b
+    def learn(self, ans):
+        while abs(self.y - ans) > 0.2:
+            if self.y > ans:
+                self.w -= 0.1
+            else:
+                self.w += 0.1
+            self.y = self.w * self.x + self.b
+        while abs(self.y - ans) > 0.05:
+            if self.y > ans:
+                self.w -= 0.02
+            else:
+                self.w += 0.02
+            self.y = self.w * self.x + self.b
+
+
+
 
 
